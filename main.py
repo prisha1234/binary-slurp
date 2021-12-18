a15f61dc8726f0e0c1bf10fdc4aae9fb51e9a9ea
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-12-18 00:44:43 +0000

    nnjbh

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 print ("THIS = BINARY SEARCH")
 def binarysearchpro(arr,start,end,x):
-if end>=start:
-mid = start + (end- start)//2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-9
+  if end >= start:
+    mid = start + (end - start)//2
+    if arr[mid] == x:
+      return mid
+    elif arr[mid] > x:
+     return binarysearchpro(arr,start,mid-1,x)
+    else:
+     return binarysearchpro(arr,start,mid-1,x)
+  else:
+     return-1
+  arr = sorted (['a','q','w','e','r','t','y','u','i','o','p'])
+  x = input ("enter the number of your choice")
+  result = binarysearchpro (arr,0,len(arr)-1,x)
+  if result != -1:
+    print ("the number is present at the index"+str(result)
+  else :
+    print("element is not in the array")
